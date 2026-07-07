3b62f959b80db73bc16ccebc886dbc14beed9f42
Sravan Karthik <sravankarthik95@gmail.com>
2026-07-07 19:57:37 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Sravan Karthik",
+    author_email="sravankarthik95@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
+
+
+
